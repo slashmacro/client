fc405b7c01b30daf4c0e1f7c274ed13e4e2caeb1
Zach Harris <zachbharris94@gmail.com>
2019-05-29 23:42:33 -0500

    macros

--- src/components/Layout/Navbar/UserLoggedIn.jsx
+++ src/components/Layout/Navbar/UserLoggedIn.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import axios from 'axios'
 
 // materia-ui
 import { makeStyles } from '@material-ui/core/styles'
@@ -9,13 +10,25 @@ import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
+import Modal from '@material-ui/core/Modal'
+import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 
 // actions
 import { userLogout } from './actions'
 
-const useStyles = makeStyles(() => ({
-  avatar: {}
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(4),
+    outline: 'none',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  }
 }))
 
 const UserLoggedIn = ({ user, userLogout }) => {
@@ -24,15 +37,33 @@ const UserLoggedIn = ({ user, userLogout }) => {
   const handleClick = e => setAnchorEl(e.currentTarget)
   const handleClose = () => setAnchorEl(null)
 
+  const [model, setModel] = useState(false)
+  const handleModalOpen = () => setModel(true)
+  const handleModalClose = () => setModel(false)
+
   // helper function to close menu before logout
   const handleLogout = async () => {
     await handleClose()
     return userLogout()
   }
 
+  const [form, setForm] = useState({ title: '', macro: '' })
+  const handleSubmit = e => {
+    e.preventDefault()
+    axios
+      .post('/api/macros', form)
+      .then(res => handleClose())
+      .catch(err => console.error(err))
+  }
+  const handleChange = e => {
+    const { name, value } = e.target
+    return setForm({ ...form, [name]: value })
+  }
+
   const classes = useStyles()
   return (
     <>
+      <Button onClick={handleModalOpen}>New Macro</Button>
       <IconButton size="small" onClick={handleClick}>
         <Avatar alt="User Name" src={user.photo} className={classes.avatar} />
       </IconButton>
@@ -44,6 +75,48 @@ const UserLoggedIn = ({ user, userLogout }) => {
           <Typography>Log Out</Typography>
         </MenuItem>
       </Menu>
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={model}
+        onClose={handleModalClose}
+      >
+        <div className={classes.paper}>
+          <Typography variant="h6" id="modal-title">
+            New Macro
+            <form onSubmit={handleSubmit}>
+              <TextField
+                id="macro-title"
+                label="Title"
+                type="text"
+                name="title"
+                autoComplete="off"
+                margin="normal"
+                variant="outlined"
+                fullWidth
+                value={form.title}
+                onChange={handleChange}
+              />
+              <TextField
+                id="macro-macro"
+                label="Macro"
+                type="text"
+                name="macro"
+                autoComplete="off"
+                margin="normal"
+                variant="outlined"
+                multiline
+                fullWidth
+                value={form.macro}
+                onChange={handleChange}
+              />
+              <Button type="submit" fullWidth>
+                Submit
+              </Button>
+            </form>
+          </Typography>
+        </div>
+      </Modal>
     </>
   )
 }
--- src/routes/Landing/index.jsx
+++ src/routes/Landing/index.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+// material-ui
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
+import Typography from '@material-ui/core/Typography'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+const useStyles = makeStyles(theme => ({
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular
+  }
+}))
 
 const LandingPage = () => {
-  return <p>landing</p>
+  const classes = useStyles()
+  const [macros, set] = useState([])
+  const getMacros = () => {
+    axios.get('/api/macros').then(res => set(res.data))
+  }
+  useEffect(() => {
+    getMacros()
+  }, [])
+  return (
+    <Container>
+      {macros.map(macro => {
+        return (
+          <ExpansionPanel key={macro._id}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography className={classes.heading}>{macro.title}</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>{macro.macro}</Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        )
+      })}
+    </Container>
+  )
 }
 
 export default LandingPage
