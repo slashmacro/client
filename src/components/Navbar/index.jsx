06a896e9ba3cdde7952eee7b69be5ed60f0f106e
Zach Harris <zachbharris94@gmail.com>
2019-12-15 02:52:05 -0600

    its alive

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { useSelector } from 'react-redux'
+import { useFirebase } from 'react-redux-firebase'
+
+import { Button, Input } from 'components/Shared'
 
 const Navbar = ({ className, ...rest }) => {
+  const firebase = useFirebase()
   const auth = useSelector(state => state.firebase.auth)
+
+  const [login, setLogin] = useState({})
+  const onChange = e => setLogin({ ...login, [e.target.name]: e.target.value })
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const { email, password } = login
+    return firebase.login({ email, password })
+  }
+
+  const logout = () => firebase.logout()
 
   return (
     <div className={className}>
       <p>slashmacro</p>
-      {auth.isEmpty ? <p>logged in</p> : <p>logged out</p>}
+      {auth.isEmpty ? (
+        <form onSubmit={handleSubmit}>
+          <Input
+            name="email"
+            label="email"
+            id="email"
+            type="email"
+            onChange={onChange}
+          />
+          <Input
+            name="password"
+            label="password"
+            id="password"
+            type="password"
+            onChange={onChange}
+          />
+          <Button type="submit">submit</Button>
+        </form>
+      ) : (
+        <Button type="button" onClick={logout}>
+          logout
+        </Button>
+      )}
     </div>
   )
 }
--- src/routes/Landing/index.jsx
+++ src/routes/Landing/index.jsx
@@ -1,52 +1,75 @@
 import React, { useState } from 'react'
-import { useFirebase } from 'react-redux-firebase'
+import { useFirestoreConnect, useFirestore } from 'react-redux-firebase'
 import { useSelector } from 'react-redux'
 
 import { Button, Input } from 'components/Shared'
+import { auth } from 'firebase'
 
 const Landing = props => {
-  const firebase = useFirebase()
+  const firestore = useFirestore()
+  useFirestoreConnect([{ collection: 'macros' }])
+  const macros = useSelector(state => state.firestore.ordered.macros)
   const auth = useSelector(state => state.firebase.auth)
 
-  const [login, setLogin] = useState({})
-  const onChange = e => setLogin({ ...login, [e.target.name]: e.target.value })
-
-  const handleSubmit = e => {
-    e.preventDefault()
-
-    const { email, password } = login
-    return firebase.login({ email, password })
+  const [form, setForm] = useState({})
+  const onChange = ({ target }) => {
+    return setForm({ ...form, [target.name]: target.value })
   }
 
-  const logout = () => firebase.logout()
+  const onSubmit = e => {
+    e.preventDefault()
+
+    const data = {
+      createdBy: auth.uid,
+      ...form,
+    }
+
+    return firestore.collection('macros').add(data)
+  }
 
   return (
     <div>
       <p>landing</p>
 
-      {auth.isEmpty ? (
-        <form onSubmit={handleSubmit}>
-          <Input
-            name="email"
-            label="email"
-            id="email"
-            type="email"
-            onChange={onChange}
-          />
-          <Input
-            name="password"
-            label="password"
-            id="password"
-            type="password"
-            onChange={onChange}
-          />
-          <Button type="submit">submit</Button>
-        </form>
-      ) : (
-        <Button type="button" onClick={logout}>
-          logout
-        </Button>
-      )}
+      <h2>Create Macro</h2>
+      <form onSubmit={onSubmit}>
+        <Input
+          name="title"
+          label="title"
+          id="title"
+          value={form.title}
+          onChange={onChange}
+        />
+        <Input
+          name="description"
+          label="description"
+          id="description"
+          value={form.description}
+          onChange={onChange}
+        />
+        <Input
+          name="macro"
+          label="macro"
+          id="macro"
+          value={form.macro}
+          onChange={onChange}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+
+      <h2>Macros</h2>
+      {macros?.map(macro => {
+        return (
+          <div key={macro.id}>
+            <h3>{macro.title}</h3>
+            <p>{macro.description}</p>
+            <code>
+              <pre>{macro.macro}</pre>
+            </code>
+            <hr />
+          </div>
+        )
+      })}
     </div>
   )
 }
